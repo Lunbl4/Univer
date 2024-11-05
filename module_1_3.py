02fe0c44c30425465dd88b5a9ac693c061f6c15f
Lunbl4 <97483613+Lunbl4@users.noreply.github.com>
2024-11-05 18:50:17 +0300

    module_1_3.py

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+# https://urban-university.pro/student/lesson/220
+
+name = "Evgeniy"
+print("Name:", name)
+age = 40
+print("Age:", age)
+age = age + 1
+print("New age:", age)
+is_student = True
+print("Is Student:", is_student)
